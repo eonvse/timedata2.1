c4d409e07247b811911b15ec1421c52bc7dcdc22
Сергей <eonvse@ya.ru>
2024-02-29 17:24:49 +0800

    Add DB Seeder Permissions

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'role.view',
+            'role.create',
+            'role.edit',
+            'role.delete',
+            'user.view',
+            'user.create',
+            'user.edit',
+            'user.delete',
+            'task.view',
+            'task.create',
+            'task.edit',
+            'task.delete'
+         ];
+
+          // Looping and Inserting Array's Permissions into Permission Table
+         foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+          }
+    }
+}
